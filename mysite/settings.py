56cfc51c8dc2f3085ddc07987bacdc0b298b156c
Korir Nancy <korjnan@gmail.com>
2019-08-21 01:35:46 +0300

    Changed the allowed hosts in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v$o#^hiy(4$=b&@ghwi2=yo#uaqrlekeqff=1g)liacqx74p#&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','korirnancy.pythonanywhere.com']
 
 
 # Application definition
